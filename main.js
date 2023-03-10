a256aae1a412ac27ec81453bd6e624062d569d0e
Elias Gomes <eliasgo221@gmail.com>
2023-03-10 10:17:31 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,59 @@
+const form = document.getElementById('form-numbers');
+const numberTwo = document.getElementById('number-two');
+let numbersValido = false;
+
+function verificaNumbers(number1, number2) {
+
+    if (number1 < number2) {
+        return resultadoNum =  true;
+    } else {
+        return resultadoNum = false;
+    }
+}
+
+form.addEventListener ('submit', function(e) {
+
+    e.preventDefault();
+
+    const numberOne = document.getElementById('number-one');
+    const mensagemSucesso = `O segundo número (${numberTwo.value}) é maior que o primeiro número (${numberOne.value}), por isso o formulário foi submetido!`;
+
+    numbersValido = verificaNumbers(numberOne.value, numberTwo.value);
+    if (numbersValido) {
+
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        numberOne.value = '';
+        numberTwo.value = '';
+
+    } 
+
+    if (!numbersValido) {
+        numberTwo.classList.add('error');
+        document.querySelector('.error-mesage').style.display = 'block';
+    }else{
+        numberTwo.classList.remove('error');
+        document.querySelector('.error-mesage').style.display = 'none';
+    }
+})
+
+
+/*else {
+    numberTwo.style.border = '1px solid red';
+    document.querySelector('.error-mesage').style.display = 'block';
+}
+numberTwo.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    numbersValido = verificaNumbers(e.target.value);
+
+    if (!numbersValido) {
+        numberTwo.classList.add('error');
+        document.querySelector('.error-mesage').style.display = 'block';
+    }else{
+        numberTwo.classList.remove('error');
+        document.querySelector('.error-mesage').style.display = 'none';
+    }
+
+})*/
