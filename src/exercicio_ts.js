e2268c994ce7285fb1bb1259bf9c44f4e8517e55
Elias Gomes <eliasgo221@gmail.com>
2023-06-29 09:26:44 -0300

    atualização exercício

--- src/exercicio_ts.js
+++ src/exercicio_ts.js
@@ -6,3 +6,12 @@ function multiplicacao(a, b) {
 // Constante para receber o resultado da multiplicação 
 const resultado = multiplicacao(2, 3);
 console.log(resultado);
+// Função para saudação
+function saudacao(nomeCompleto) {
+    const nome = nomeCompleto.split(' ');
+    const primeiroNome = nome[0];
+    console.log(`Olá ${primeiroNome}`);
+}
+// Constantes para receber nome e colocar o nome na função de saudação 
+const nomeCompleto = 'Elias Gomes De Oliveira Júnior';
+const saudacaoMensagem = saudacao(nomeCompleto);
