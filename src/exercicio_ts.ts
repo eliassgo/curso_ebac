033cf89b50eeefc42702bcf8beb8c6f2787f0a99
Elias Júnior <120608166+eliassgo@users.noreply.github.com>
2023-06-29 09:15:57 -0300

    Update exercicio_ts.ts

--- src/exercicio_ts.ts
+++ src/exercicio_ts.ts
@@ -8,6 +8,16 @@ function multiplicacao(a: number, b: number): number {
 const resultado: number = multiplicacao(2, 3);
 console.log(resultado);
 
+// Função para saudação
+function saudacao(nomeCompleto: string): void {
+    const nome = nomeCompleto.split(' ');
+    const primeiroNome = nome[0];
+    console.log(`Olá ${primeiroNome}`)
+}
+
+// Constantes para receber nome e colocar o nome na função de saudação 
+const nomeCompleto = 'Elias Gomes De Oliveira Júnior';
+const saudacaoMensagem = saudacao(nomeCompleto);
 
 
 
